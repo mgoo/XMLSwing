aa9478ed62fecbd0cefa21e3a6d6c6588dd4b190
mgoo <andrewmcghienz@gmail.com>
2016-08-02 09:22:25 +1200

    added create mthods that takes file and scanner

--- src/XMLReader/XMLReader.java
+++ src/XMLReader/XMLReader.java
@@ -13,5 +13,13 @@ public class XMLReader {
 	public XMLCursor openXMLFile(String fileName) throws IOException{
 		return new XMLCursor(new Scanner(new File(fileName)));
 	}
+	
+	public XMLCursor openXMLFileFromFile(File file) throws IOException{
+		return new XMLCursor(new Scanner(file));
+	}
+	
+	public XMLCursor openXMLFileFromScanner(Scanner fileScanner) throws IOException{
+		return new XMLCursor(fileScanner);
+	}
 
 }
